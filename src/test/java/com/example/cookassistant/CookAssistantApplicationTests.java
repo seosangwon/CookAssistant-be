d8f9a5cebe2a01e3e157aea8c31f11ac88881c77
seosang-won <zmdk1205@naver.com>
2024-03-26 18:50:46 +0900

    init

---
+++ src/test/java/com/example/cookassistant/CookAssistantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cookassistant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CookAssistantApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

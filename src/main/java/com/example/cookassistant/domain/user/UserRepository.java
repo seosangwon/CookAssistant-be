8f053386fb0fc2eda97294776b42a7957fb235f2
seosang-won <zmdk1205@naver.com>
2024-04-17 15:54:12 +0900

    🌟[Feature] Add Simple User ad-hoc API (only create)

--- src/main/java/com/example/cookassistant/domain/user/UserRepository.java
+++ src/main/java/com/example/cookassistant/domain/user/UserRepository.java
@@ -3,9 +3,12 @@ package com.example.cookassistant.domain.user;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<User , Long> {
 
 
+    Optional<User> findByEmail(String email);
 
 }
